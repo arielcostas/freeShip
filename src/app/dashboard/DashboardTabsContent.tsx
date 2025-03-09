6a69e911f2ea1aa01fc1a88a06bda6fc20629b56
Dominique Haroun <haroundominique@gmail.com>
2025-03-09 17:14:18 +0100

    eliminando div sobrante en dashboard

--- src/app/dashboard/DashboardTabsContent.tsx
+++ src/app/dashboard/DashboardTabsContent.tsx
@@ -9,16 +9,12 @@ export function DashboardTabsContent({
   userId: string;
 }) {
   return (
-    <div className="flex-grow h-full w-full p-4 bg-white shadow-md rounded-lg mt-2">
+    <>
       {activeTab === "misProyectos" ? (
-        <div className="flex-grow h-full w-full bg-white p-4 rounded-lg shadow-md flex flex-col overflow-hidden">
-          <MyProjectsDashboardView userId={userId} />
-        </div>
+        <MyProjectsDashboardView userId={userId} />
       ) : (
-        <div className="flex-grow h-full w-full bg-white p-4 rounded-lg shadow-md flex flex-col overflow-hidden">
-          <OtherProjectsDashboardView userId={userId} />
-        </div>
+        <OtherProjectsDashboardView userId={userId} />
       )}
-    </div>
+    </>
   );
 }
--- src/app/dashboard/projects/my-projects/MyProjectsDashboardView.tsx
+++ src/app/dashboard/projects/my-projects/MyProjectsDashboardView.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 import MyProjectsList from "@/app/dashboard/projects/my-projects/MyProjectsList";
 
 export default async function MyProjectsDashboardView({
-  userId,
-}: {
+                                                        userId,
+                                                      }: {
   userId: string;
 }) {
   return (
@@ -12,7 +12,7 @@ export default async function MyProjectsDashboardView({
         <div className="flex justify-start">
           <Link
             href="/dashboard/projects/my-projects/new"
-            className="bg-[#acd916] text-gray-700 px-4 font-bold py-2 rounded hover:bg-[#88b000] hover:text-white transition"
+            className="bg-[#acd916] text-gray-700 px-4 font-bold py-2 rounded hover:bg-[#88b000] hover:text-white transition ml-4 mt-4"
           >
             Nuevo proyecto
           </Link>
