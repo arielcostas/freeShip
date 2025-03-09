bc8cced46e47602c673da51806d8672ffc5aa669
Dominique Haroun <haroundominique@gmail.com>
2025-03-09 17:28:39 +0100

    probando el mostrar div o no segun dispositivo

--- src/app/dashboard/DashboardTabsContent.tsx
+++ src/app/dashboard/DashboardTabsContent.tsx
@@ -9,16 +9,28 @@ export function DashboardTabsContent({
   userId: string;
 }) {
   return (
-    <div className="flex-grow h-full w-full p-4 bg-white shadow-md rounded-lg mt-2">
-      {activeTab === "misProyectos" ? (
-        <div className="flex-grow h-full w-full bg-white p-4 rounded-lg shadow-md flex flex-col overflow-hidden">
+    <>
+      {/* Este contenido será visible en pantallas móviles */}
+      <div className="md:hidden bg-gray-100 p-4 rounded-lg shadow-md">
+        {activeTab === "misProyectos" ? (
           <MyProjectsDashboardView userId={userId} />
-        </div>
-      ) : (
-        <div className="flex-grow h-full w-full bg-white p-4 rounded-lg shadow-md flex flex-col overflow-hidden">
+        ) : (
           <OtherProjectsDashboardView userId={userId} />
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+
+      {/* Este contenido será visible solo en pantallas de PC */}
+      <div className="hidden md:block flex-grow h-full w-full p-4 bg-white shadow-md rounded-lg mt-2">
+        {activeTab === "misProyectos" ? (
+          <div className="flex-grow h-full w-full bg-white p-4 rounded-lg shadow-md flex flex-col overflow-hidden">
+            <MyProjectsDashboardView userId={userId} />
+          </div>
+        ) : (
+          <div className="flex-grow h-full w-full bg-white p-4 rounded-lg shadow-md flex flex-col overflow-hidden">
+            <OtherProjectsDashboardView userId={userId} />
+          </div>
+        )}
+      </div>
+    </>
   );
 }
