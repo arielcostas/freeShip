7a44989316389cd8d04d00cfc7e5f0a7e966088a
Dominique Haroun <haroundominique@gmail.com>
2025-03-09 17:22:46 +0100

    prueba

--- src/app/dashboard/DashboardTabsContent.tsx
+++ src/app/dashboard/DashboardTabsContent.tsx
@@ -2,31 +2,23 @@ import MyProjectsDashboardView from "@/app/dashboard/projects/my-projects/MyProj
 import OtherProjectsDashboardView from "@/app/dashboard/projects/other/OtherProjectsDashboardView";
 
 export function DashboardTabsContent({
-                                       activeTab,
-                                       userId,
-                                     }: {
+  activeTab,
+  userId,
+}: {
   activeTab: string;
   userId: string;
 }) {
   return (
-    <>
-      {/* Este contenido será visible en pantallas móviles */}
-      <div className="md:hidden bg-gray-100">
-        {activeTab === "misProyectos" ? (
+    <div className="flex-grow h-full w-full p-4 bg-white shadow-md rounded-lg mt-2">
+      {activeTab === "misProyectos" ? (
+        <div className="flex-grow h-full w-full bg-white p-4 rounded-lg shadow-md flex flex-col overflow-hidden">
           <MyProjectsDashboardView userId={userId} />
-        ) : (
+        </div>
+      ) : (
+        <div className="flex-grow h-full w-full bg-white p-4 rounded-lg shadow-md flex flex-col overflow-hidden">
           <OtherProjectsDashboardView userId={userId} />
-        )}
-      </div>
-
-      {/* Este contenido será visible solo en pantallas de PC */}
-      <div className="hidden md:block">
-        {activeTab === "misProyectos" ? (
-          <MyProjectsDashboardView userId={userId} />
-        ) : (
-          <OtherProjectsDashboardView userId={userId} />
-        )}
-      </div>
-    </>
+        </div>
+      )}
+    </div>
   );
 }
