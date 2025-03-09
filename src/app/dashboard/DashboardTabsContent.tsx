28a21f7d00f78161db948e98e9c76b69d358d0d2
Dominique Haroun <haroundominique@gmail.com>
2025-03-09 17:20:06 +0100

    aniadiendo el div en dashbord de nuevo pero solo cuando se ve desde movil

--- src/app/dashboard/DashboardTabsContent.tsx
+++ src/app/dashboard/DashboardTabsContent.tsx
@@ -2,19 +2,31 @@ import MyProjectsDashboardView from "@/app/dashboard/projects/my-projects/MyProj
 import OtherProjectsDashboardView from "@/app/dashboard/projects/other/OtherProjectsDashboardView";
 
 export function DashboardTabsContent({
-  activeTab,
-  userId,
-}: {
+                                       activeTab,
+                                       userId,
+                                     }: {
   activeTab: string;
   userId: string;
 }) {
   return (
     <>
-      {activeTab === "misProyectos" ? (
-        <MyProjectsDashboardView userId={userId} />
-      ) : (
-        <OtherProjectsDashboardView userId={userId} />
-      )}
+      {/* Este contenido será visible en pantallas móviles */}
+      <div className="md:hidden bg-gray-100">
+        {activeTab === "misProyectos" ? (
+          <MyProjectsDashboardView userId={userId} />
+        ) : (
+          <OtherProjectsDashboardView userId={userId} />
+        )}
+      </div>
+
+      {/* Este contenido será visible solo en pantallas de PC */}
+      <div className="hidden md:block">
+        {activeTab === "misProyectos" ? (
+          <MyProjectsDashboardView userId={userId} />
+        ) : (
+          <OtherProjectsDashboardView userId={userId} />
+        )}
+      </div>
     </>
   );
 }
