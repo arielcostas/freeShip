3537e21de18db1352e7e6aaf87a43a229e2a2ca5
haroundominique <haroundominique@gmail.com>
2025-03-23 13:26:53 +0100

    cambiando titulo uno por ciento

--- src/app/dashboard/projects/hall/HallDashboardView.tsx
+++ src/app/dashboard/projects/hall/HallDashboardView.tsx
@@ -59,8 +59,8 @@ export default function HallDashboardView() {
       {/* Versión para PC */}
       {!isMobile && (
         <div className="pl-20 overflow-y-auto max-h-[calc(100vh-250px)]">
-          <h2 className="text-3xl font-bold mb-6 text-center pr-20">
-            1%
+          <h2 className="text-5xl font-bold mb-6 text-center pr-20">
+            ⇀ 1% ↼
           </h2>
           <ul className="space-y-6 relative">
             {projects.map((project, index) => (
